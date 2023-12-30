a95a95f218cc5e953583fc4e5a830e41564e6002
BjornFJohansson <bjornjobb@gmail.com>
2023-12-30 15:48:40 +0000

    sphinx docs set up

--- docs/conf.py
+++ docs/conf.py
@@ -7,32 +7,36 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'seguid'
-copyright = '2023, Björn F. Johansson'
-author = 'Björn F. Johansson'
-release = '0.0.1'
+copyright = '2023, bjorn'
+author = 'bjorn'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.coverage',
-              'sphinx.ext.napoleon',
-              "sphinx.ext.doctest",
-              "sphinx.ext.viewcode",
-              "sphinx.ext.autosummary",
-              "numpydoc",
-              "sphinx.ext.intersphinx",]
-
-intersphinx_mapping = {
-    "biopython": ("https://biopython.org/docs/latest/api/", None),
-    "python": ("http://docs.python.org/3.8", None),
-}
+			  'sphinx.ext.coverage',
+			  'sphinx.ext.napoleon',
+			  "sphinx.ext.doctest",
+			  "sphinx.ext.viewcode",
+			  "sphinx.ext.autosummary",
+			  "numpydoc",
+			  "sphinx.ext.intersphinx",]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "tests.py"]
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+intersphinx_mapping = {
+	"biopython": ("https://biopython.org/docs/latest/api/", None),
+	"python": ("http://docs.python.org/3.8", None),
+}
 
 autodoc_member_order = 'bysource'
 autodoc_preserve_defaults = True
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
--- docs/tests.rst
+++
@@ -1,4 +0,0 @@
-.. automodule:: tests
-   :members:
-   :undoc-members:
-   :show-inheritance:
