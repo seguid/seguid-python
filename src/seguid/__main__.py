9d94246c9106f3652ad84af37ee4e2354cfccae9
BjornFJohansson <bjornjobb@gmail.com>
2024-02-10 08:34:13 +0000

    added form arg

--- src/seguid/__main__.py
+++ src/seguid/__main__.py
@@ -13,12 +13,18 @@ parser = ArgumentParser(prog="python -m seguid", description="seguid: Sequence G
 parser.add_argument("--version", action="store_true", help="Show version")
 parser.add_argument("--table", type=str, nargs="?", help="Type of input sequence")
 parser.add_argument("--type", type=str, nargs="?", help="Type of checksum to calculate")
+parser.add_argument("--form", type=str, nargs="?", help="Form of checksum to return")
+
 
 args = vars(parser.parse_args())
 
 if args.pop("version"):
     print(__version__)
 else:
+    form=args.pop("form")
+    if form == None:
+        form="long"
+
     type=args.pop("type")
     if type == None:
         type="seguid"
@@ -40,17 +46,17 @@ else:
     seq="\n".join(lines)
 
     if type == "seguid":
-        res=seguid(seq, table = table)
+        res=seguid(seq, table = table, form = form)
     elif type == "lsseguid":
-        res=lsseguid(seq, table = table)
+        res=lsseguid(seq, table = table, form = form)
     elif type == "csseguid":
-        res=csseguid(seq, table = table)
+        res=csseguid(seq, table = table, form = form)
     elif type == "ldseguid":
         tuple=tuple_from_repr(seq, table = table)
-        res=ldseguid(watson = tuple[0], crick = tuple[1], overhang = tuple[2], table = table)
+        res=ldseguid(watson = tuple[0], crick = tuple[1], overhang = tuple[2], table = table, form = form)
     elif type == "cdseguid":
         tuple=tuple_from_repr(seq, table = table)
-        res=cdseguid(watson = tuple[0], crick = tuple[1], table = table)
+        res=cdseguid(watson = tuple[0], crick = tuple[1], table = table, form = form)
     else:
         raise ValueError("Unknown --type='" + type + "'")
 
