7c1b9f24b8aa6bf22badd493e2e2f35da9902f64
BjornFJohansson <bjornjobb@gmail.com>
2024-03-02 09:01:39 +0000

    added version in python3.6,3.7

--- src/seguid/__init__.py
+++ src/seguid/__init__.py
@@ -2,7 +2,13 @@ try:
     # importlib only avaliable in Python 3.8 and up
     from importlib.metadata import version as _version
 except ModuleNotFoundError:
-    __version__ = "0.0.0"
+    try:
+        # pkg_resources avaliable in Python 3.6-3.7 from setuptools =< 59.6.0
+        import pkg_resources
+    except ModuleNotFoundError:
+        __version__ = "0.0.0"
+    else:
+        __version__ = pkg_resources.get_distribution(__package__).version
 else:
     __version__ = _version(__package__)
 
