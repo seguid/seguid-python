47c95cfaf5f1cd6395efdd737bda93023c44c3fe
BjornFJohansson <bjornjobb@gmail.com>
2024-04-21 08:40:04 +0100

    formatted numbered refs in module level docstring

--- src/seguid/__init__.py
+++ src/seguid/__init__.py
@@ -1,41 +1,120 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Four functions for calculations of SEGUID checksums for biological sequences with varying topologies.
+SEGUID checksums for linear, circular, single- and double-stranded sequences
+----------------------------------------------------------------------------
 
-================ ================ ============== ====================
- Function         Topology         Strandedness   Algorithm
-================ ================ ============== ====================
- ``lsseguid()``   **l**\ inear     *s*\ ingle     **l**\ *s*\ SEGUID
- ``csseguid()``   **c**\ ircular   *s*\ ingle     **c**\ *s*\  "
- ``ldseguid()``   **l**\ inear     *d*\ ouble     **l**\ *d*\  "
- ``cdseguid()``   **c**\ ircular   *d*\ ouble     **c**\ *d*\  "
-================ ================ ============== ====================
+================ ============== ====================
+ Topology         Strandedness   Function
+================ ============== ====================
+ **l**\ inear     **s**\ ingle     ``lsseguid()``
+ **c**\ ircular   **s**\ ingle     ``csseguid()``
+ **l**\ inear     **d**\ ouble     ``ldseguid()``
+ **c**\ ircular   **d**\ ouble     ``cdseguid()``
+================ ============== ====================
 
+Four functions for calculations of SEGUID v2 checksums for biological sequences with
+linear or circular topologies and a single of two complementary strands are provided.
 
-
-A detailed description of the algorithms can be found in Pereira et al. 2024.
+A detailed description of the algorithms can be found in Pereira et al. 2024 (1).
 Implementations of these functions in other programming languages cand be found
 at `seguid.org <https://www.seguid.org/>`_.
 
-The original SEGUID v1 (Babnigg & Giometti, 2006) :func:`seguid.seguid` is also provided along
-with some auxillary functions.
+A function ``seguid()`` that calculates checksum according to the original SEGUID v1 specification (2)
+is also provided.
 
-This package works without external dependencies, but csseguid and cdseguid
+This package works without external dependencies, but ``csseguid()`` and ``cdseguid()``
 can be made faster by installing `pydivsufsort <https://pypi.org/project/pydivsufsort/>`_
-since a faster algorithm for finding the
-`smallest string rotation <https://en.wikipedia.org/wiki/Lexicographically_minimal_string_rotation>`_ is provided.
+since pydivsufsort provides a faster algorithm for finding the
+`smallest string rotation <https://en.wikipedia.org/wiki/Lexicographically_minimal_string_rotation>`_.
+
+
+Usage
+-----
+
+.. code-block:: python
+
+    lsseguid(seq,alphabet="{DNA}",form=c("long","short","both"))
+
+    csseguid(seq,alphabet="{DNA}",form=c("long","short","both"))
+
+    ldseguid(watson,crick,alphabet="{DNA}",form=c("long","short","both"))
+
+    cdseguid(watson,crick,alphabet="{DNA}",form=c("long","short","both"))
+
+    seguid(seq,alphabet="{DNA}",form=c("long","short","both"))
+
+
+Function arguments
+------------------
+
+**seq** (character string)
+The sequence for which the checksum should be calculated. The sequence may only comprise of symbols in the
+alphabet specified by the alphabet argument.
+
+**alphabet** (character string)
+The type of sequence used. If "``{DNA}``" (default), then the input is a DNA sequence. If "``{RNA}``", then the
+input is an RNA sequence. If "``{protein}``", then the input is an amino-acid sequence. If "``{DNA-extended}``"
+or "``{RNA-extended}``", then the input is a DNA or RNA sequence specified an extended set of symbols, including
+IUPAC symbols (4). If "``{protein-extended}``", then the input is an amino-acid sequence with an extended set
+of symbols, including IUPAC symbols (5). A custom alphabet may also be used. A non-complementary alphabet
+is specified as a comma-separated set of single symbols, e.g. "``X,Y,Z``". A complementary alphabet is specified
+as a comma-separated set of paired symbols, e.g. "``AT,CG``". It is also possible to extend a pre-defined
+alphabet, e.g. "``{DNA},XY``".
+
+**form** (character string)
+How the checksum is presented. If "``long``" (default), the full-length checksum is returned. If "``short``",
+the short, six-digit checksum is returned. If "``both``", both the short and the long checksums are returned.
+
+**watson, crick** (character strings)
+Two reverse-complementary DNA sequences. Both sequences should be specified in the 5'-to-3' direction.
+
+**Value** The SEGUID functions return a single character string, if form is either "``long``" or "``short``". If form
+is "``both``", then a tuple of length two is returned, where the first component holds the "``short``"
+checksum and the second the "long" checksum. The long checksum, without the prefix, is string with 27
+characters. The short checksum, without the prefix, is the first six characters of the long checksum.
+All checksums are prefixed with a label indicating which SEGUID method was used. Except for seguid(),
+which uses base64 encoding, all functions produce checksums using the base64url encoding
+("Base 64 Encoding with URL and Filename Safe Alphabet").
+
+
+Base64 and Base64url encodings
+The base64url encoding is the base64 encoding with non-URL-safe characters substituted with URL-safe ones (3).
+Specifically, the plus symbol (+) is replaced by the minus symbol (-), and the forward slash (/) is replaced
+by the underscore symbol (_).
+
+The Base64 checksum used for the original SEGUID checksum is not guaranteed to contain symbols
+that can safely be used as-is in a Uniform Resource Locator (URL). Specifically, it may consist of forward
+slashes (/) and plus symbols (+), which are characters that carry special meaning in a URL. For the same
+reason, a Base64 checksum cannot safely be used as a file or directory name, because it may have a forward slash.
+
+The checksum returned is always 27-character long. This is because the SHA-1 hash (6) is 160-bit long
+(20 bytes), which result in the encoded representation always end with a padding character (=) so that the
+length is a multiple of four character. We relax this requirement, by dropping the padding character.
+
+
+
+
 
 References
 ----------
-* Pereira, Humberto, Paulo César Silva, Wayne M Davis, Louis Abraham, Gyorgy Babnigg,
-  Henrik Bengtsson, and Bjorn Johansson. 2024. “SEGUID v2: Extending SEGUID Checksums
-  for Circular, Linear, Single- and Double-Stranded Biological Sequences.” bioRxiv.
-  https://doi.org/10.1101/2024.02.28.582384.
+1. Pereira, Humberto, Paulo César Silva, Wayne M Davis, Louis Abraham, Gyorgy Babnigg,
+   Henrik Bengtsson, and Bjorn Johansson. 2024. “SEGUID v2: Extending SEGUID Checksums
+   for Circular, Linear, Single- and Double-Stranded Biological Sequences.” bioRxiv.
+   https://doi.org/10.1101/2024.02.28.582384.
 
-* Babnigg, G., & Giometti, C. S. (2006). A database of unique protein
-  sequence identifiers for proteome studies. Proteomics, 6(16), 4514–4522.
-  https://doi.org/10.1002/pmic.200600032
+2. Babnigg, G., & Giometti, C. S. (2006). A database of unique protein
+   sequence identifiers for proteome studies. Proteomics, 6(16), 4514–4522.
+   https://doi.org/10.1002/pmic.200600032
+
+3. S Josefsson, The Base16, Base32, and Base64 Data Encodings, RFC 4648, October 2006, doi:10.17487/RFC4648 .
+
+4. Wikipedia article 'Nucleic acid notation', February 2024, https://en.wikipedia.org/wiki/Nucleic_acid_notation .
+
+5. Wikipedia article 'Amino acids', February 2024, https://en.wikipedia.org/wiki/Amino_acid .
+
+6. Wikipedia article 'SHA-1' (Secure Hash Algorithm 1), December 2023, https://en.wikipedia.org/wiki/SHA-1 .
+
 
 """
 
