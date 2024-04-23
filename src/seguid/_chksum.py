6c2c7dd434cfe1b62d019c4c95f73612a1d1af35
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-04-23 08:13:29 -0700

    grammar: paragraph

--- src/seguid/_chksum.py
+++ src/seguid/_chksum.py
@@ -73,8 +73,7 @@ def seguid(seq: str, alphabet: str = "{DNA}", form: str = "long") -> str:
 
     The original definition of the SEGUID v1 checksum algorithm (Babnigg & Giometti, 2006)
     included transformation to uppercase before calculating the checksum.
-
-    ``seguid()`` does *not* coerce the input sequence to upper case. If your input sequence
+    Here, ``seguid()`` does *not* coerce the input sequence to upper case. If your input sequence
     has lower-case symbols, you can use :meth:`str.upper` to emulate what the original method does.
     ``seguid()`` only accepts symbols as specified by the `alphabet` argument.
     Thus, our implementation is more conservative, which has the benefit of
